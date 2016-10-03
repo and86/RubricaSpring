6b546152a3108c1158e2ffc0458b0f30843b7a02
Greis92 <graziatorino62@gmail.com>
2016-10-03 12:05:56 +0200

    Inizio progetto Rubrica

---
+++ Rubrica/src/Rubrica/Rubrica.java
@@ -0,0 +1,23 @@
+package Rubrica;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Rubrica {
+
+		private Map<String,Voce> voci = new TreeMap<String,Voce>(); 
+		
+		
+		public Voce aggiungiVoce(String nome,String cognome,String tel) throws VoceGiaEsiste{
+			
+			if(voci.containsKey(nome+" "+cognome)){
+				throw new VoceGiaEsiste("Voce Esistente in Rubrica!");
+				
+			}
+			Voce v = new Voce(nome,cognome,tel); // SE NON ESISTE, CREO NUOVA VOCE
+			voci.put(nome+" "+cognome, v); // INSERISCO IN RUBRICA
+			
+			return v;
+		}
+		
+}
---
+++ Rubrica/src/Rubrica/VoceGiaEsiste.java
@@ -0,0 +1,14 @@
+package Rubrica;
+
+public class VoceGiaEsiste extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public VoceGiaEsiste(String s){
+		super(s);
+	}
+	
+}
